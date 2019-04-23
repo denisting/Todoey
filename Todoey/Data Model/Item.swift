212f658b7632b3da76b02dbf8a8a1e2d9f4efd89
Denis Rodrigues <denis.sting@gmail.com>
2019-04-23 13:13:34 -0400

    Covert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Denis Rodrigues on 4/22/19.
+//  Copyright © 2019 Denis Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title: String = ""
+    
+    var done: Bool = false
+}
